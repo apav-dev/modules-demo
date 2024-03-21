eff6165394d1b6a5528cc97010ac20803aa0f018
apav-dev <apavlick@yext.com>
2024-03-21 16:30:52 -0400

    your-module module

---
+++ src/modules/your-module/your-module.tsx
@@ -0,0 +1,36 @@
+import { Module, ModuleConfig, ModuleProps } from "@yext/pages/*";
+import { AnalyticsProvider } from "@yext/pages-components";
+import "./index.css";
+
+const templateData: ModuleProps = {
+  document: {
+    businessId: "REPLACE_ME",
+    siteId: "REPLACE_ME",
+    __: {
+      name: "your-module",
+      staticPage: true,
+    },
+  },
+  __meta: {
+    mode: "production",
+  },
+};
+
+export const config: ModuleConfig = {
+  name: "your-module",
+};
+
+const YourModule: Module = () => {
+  return (
+    <AnalyticsProvider
+      // apiKey="REPLACE_ME"
+      // defaultCurrency="REPLACE_ME"
+      templateData={templateData}
+      productionDomains={["REPLACE_ME"]}
+    >
+      <div className="tailwind">Module</div>
+    </AnalyticsProvider>
+  );
+};
+
+export default YourModule;
